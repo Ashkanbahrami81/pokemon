c5d28b7cce097d7a69d9fa24ebe576885546b9cc
ashkan <ashkanbahrami81@gmail.com>
2024-04-17 01:40:38 +0330

    Added card for pokemon details

--- src/components/atomic/organs/pokemonDetails/PokemonDetails.tsx
+++ src/components/atomic/organs/pokemonDetails/PokemonDetails.tsx
@@ -25,7 +25,7 @@ const PokemonDetails = ({
   weight,
 }: PokemonDetailsProps) => {
   return (
-    <div className="w-full flex items-center justify-center gap-3">
+    <div className="w-full flex items-center justify-center gap-3 bg-gray-950 border border-gray-500 rounded-3xl">
       <Image
         src={sprites.front_default}
         alt={name}
@@ -34,6 +34,7 @@ const PokemonDetails = ({
         style={{
           imageRendering: "pixelated", // This will make the image sharper
         }}
+        className="w-1/2 max-w-[400px]"
       />
       <PokemonInfo
         name={name}
